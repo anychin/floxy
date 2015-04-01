3497aae23a530867e4a95e336d9847fa47980652
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-04-01 09:45:55 +0300

    deploy from github

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,8 @@ set :term_mode, nil
 
 set :deploy_to, "/home/#{user}/floxy/#{rails_env}"
 set :app_path, "#{deploy_to}/#{current_path}"
-set :repository, 'git@bitbucket.org:webiumdigital/floxy_prototype.git'
+#set :repository, 'git@bitbucket.org:webiumdigital/floxy_prototype.git'
+set :repository, 'git@github.com:WebiumDigital/floxy.git'
 set :branch, ENV['BRANCH'] || 'master'
 set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'tmp']
 set :keep_releases, 10
