ad668a30adf2a0c9886ae117068b169b8b8fecb0
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-04-24 11:43:45 +0300

    empty current_user link in organizations#index

--- config/user_navigation.rb
+++ config/user_navigation.rb
@@ -10,6 +10,8 @@ SimpleNavigation::Configuration.run do |navigation|
     if current_user.present?
       if self.respond_to?(:current_organization) and current_organization.present?
         primary.item :user, current_user, organization_member_path(current_organization, current_user), highlights_on: %r(/me)
+      else
+        primary.item :user, current_user, '#'
       end
       primary.item :logout, 'Выйти', destroy_user_session_path, method: "delete"#, class: 'btn btn-danger'
       # primary.item :settings, 'Настройки', organization_settings_url
