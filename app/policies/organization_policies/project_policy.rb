aa9db389576a49ac91286a30fa95a8a50ee5c977
Alexander Logunov <unlovedru@gmail.com>
2015-04-24 12:14:40 +0300

    project policy show fix

--- app/policies/organization_policies/project_policy.rb
+++ app/policies/organization_policies/project_policy.rb
@@ -4,7 +4,7 @@ class OrganizationPolicies::ProjectPolicy < OrganizationPolicies::BasePolicy
   end
 
   def show?
-    organization.owner?(user) or record.team.members.include?(user)
+    organization.owner?(user) or record.team.manager?(user) or record.team.members.include?(user)
   end
 
   def update?
