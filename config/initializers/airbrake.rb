50a77d603b9e8fb6da95ed6cbec9d2b9c8f23605
Alexander Logunov <unlovedru@gmail.com>
2015-04-29 13:38:22 +0300

    airbrake update

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = '34108a7a3546d3018bfe73b74e937db1'
+  config.api_key = '3e1f94cecadb49ceeb0fd8216160b8b0'
+  config.host    = 'errbit.tinkerdev.ru'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
