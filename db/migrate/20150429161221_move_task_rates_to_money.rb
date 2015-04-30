4eb7476fad3afe121b76be8a2bffa11a096e3941
Alexander Logunov <unlovedru@gmail.com>
2015-04-30 11:19:18 +0300

    update task money in single migration

--- db/migrate/20150429161221_move_task_rates_to_money.rb
+++ db/migrate/20150429161221_move_task_rates_to_money.rb
@@ -5,11 +5,5 @@ class MoveTaskRatesToMoney < ActiveRecord::Migration
 
     change_column :tasks, :stored_rate_value_cents, :integer
     change_column :tasks, :stored_client_rate_value_cents, :integer
-
-    Task.find_each do |t|
-      t.stored_rate_value_cents = t.stored_rate_value_cents * 100 if t.stored_rate_value_cents.present?
-      t.stored_client_rate_value_cents = t.stored_client_rate_value_cents * 100 if t.stored_client_rate_value_cents.present?
-      t.save
-    end
   end
 end
--- db/migrate/20150429163201_move_task_costs_to_money.rb
+++ db/migrate/20150429163201_move_task_costs_to_money.rb
@@ -5,12 +5,5 @@ class MoveTaskCostsToMoney < ActiveRecord::Migration
 
     change_column :tasks, :stored_cost_cents, :integer
     change_column :tasks, :stored_client_cost_cents, :integer
-
-
-    Task.find_each do |t|
-      t.stored_cost_cents = t.stored_cost_cents*100 if t.stored_cost_cents.present?
-      t.stored_client_cost_cents = t.stored_client_cost_cents * 100 if t.stored_client_cost_cents.present?
-      t.save
-    end
   end
 end
