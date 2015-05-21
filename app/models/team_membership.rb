1b6feeb89861df1b3ca3b5401db48eab3971a286
Alexander Logunov <unlovedru@gmail.com>
2015-05-22 00:14:54 +0300

    роль team_lead_manager

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -1,5 +1,5 @@
 class TeamMembership < ActiveRecord::Base
-  ROLES = {:member=>0, :team_lead=>1, :account_manager=>2}
+  ROLES = {:member=>0, :team_lead=>1, :account_manager=>2, :team_lead_manager=>3}
 
   belongs_to :user, inverse_of: :team_memberships
   belongs_to :team, inverse_of: :team_memberships
@@ -10,5 +10,7 @@ class TeamMembership < ActiveRecord::Base
   enum role: ROLES
 
   scope :by_user, ->(user) {where(user_id: user.id)}
-  scope :managers, ->{where(role: [ROLES[:account_manager], ROLES[:team_lead]])}
+  scope :administrative, ->{where(role: [ROLES[:account_manager], ROLES[:team_lead], ROLES[:team_lead_manager]])}
+  scope :managers, ->{where(role: [ROLES[:account_manager], ROLES[:team_lead_manager]])}
+  scope :team_leads, ->{where(role: [ROLES[:team_lead], ROLES[:team_lead_manager]])}
 end
