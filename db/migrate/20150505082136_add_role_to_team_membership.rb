5f1a8ea3542cedda1cd85ded20543a2a6a62fc6b
Anton Anuchin <anychin@gmail.com>
2015-07-13 18:21:09 +0300

    off change operations whith data in migrations

--- db/migrate/20150505082136_add_role_to_team_membership.rb
+++ db/migrate/20150505082136_add_role_to_team_membership.rb
@@ -2,17 +2,17 @@ class AddRoleToTeamMembership < ActiveRecord::Migration
   def change
     add_column :team_memberships, :role, :integer, :null=>false, :default=>0
 
-    Team.find_each do |t|
-      [:team_lead, :account_manager].each do |role|
-        person_id = t.send("#{role}_id")
-        person = User.where(id: person_id).first
-        if person.present?
-          t_m = t.team_memberships.where(user: person).first_or_create
-          t_m.role = role
-          t_m.save
-        end
-      end
-    end
+    #Team.find_each do |t|
+    #  [:team_lead, :account_manager].each do |role|
+    #    person_id = t.send("#{role}_id")
+    #    person = User.where(id: person_id).first
+    #    if person.present?
+    #      t_m = t.team_memberships.where(user: person).first_or_create
+    #      t_m.role = role
+    #      t_m.save
+    #    end
+    #  end
+    #end
 
     remove_column :teams, :team_lead_id
     remove_column :teams, :account_manager_id
--- db/migrate/20150507133444_add_costs_to_user_invoices.rb
+++ db/migrate/20150507133444_add_costs_to_user_invoices.rb
@@ -4,9 +4,9 @@ class AddCostsToUserInvoices < ActiveRecord::Migration
     add_column :user_invoices, :team_lead_cost_cents, :integer
     add_column :user_invoices, :account_manager_cost_cents, :integer
 
-    UserInvoice.find_each do |ui|
-      ui.executor_cost = ui.tasks.map(&:executor_cost).compact.inject(:+)
-      ui.save
-    end
+    #UserInvoice.find_each do |ui|
+    #  ui.executor_cost = ui.tasks.map(&:executor_cost).compact.inject(:+)
+    #  ui.save
+    #end
   end
 end
