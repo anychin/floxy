6cfc94d5a81612e5e5bc0e1439d78412c61c90bf
Nikita <nmatyukov.dev@gmail.com>
2015-07-17 08:44:28 +0300

    #1096: Customer name_id validations. Green tests.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ActiveRecord::Base
   acts_as_paranoid
+
+  validates :name_id,
+    presence: true,
+    length: { minimum: 4, maximum: 9 },
+    format: { with: /[a-zA-Z0-9_]/}
 end
