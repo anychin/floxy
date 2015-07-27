4992e2e7989d90b6db7e428b8e8affc2163ff411
Nikita <nmatyukov.dev@gmail.com>
2015-07-27 19:30:02 +0300

    #1191: Display stored cost executor and team lead.

--- app/models/task.rb
+++ app/models/task.rb
@@ -180,11 +180,11 @@ class Task < ActiveRecord::Base
   end
 
   def user_invoice_executor_summary
-    "#{title} / #{planned_time} / #{executor_cost}"
+    "#{title} / #{planned_time} / #{Money.new(stored_executor_cost_cents, stored_currency)}"
   end
 
   def user_invoice_team_lead_summary
-    "#{title} / #{planned_time} / #{team_lead_cost}"
+    "#{title} / #{planned_time} / #{Money.new(stored_team_lead_cost_cents, stored_currency)}"
   end
 
   def user_invoice_account_manager_summary
