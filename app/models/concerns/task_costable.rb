d00d23a8ba06fa43321183a560a5786216db8394
Nikita <nmatyukov.dev@gmail.com>
2015-07-28 09:22:28 +0300

    #1190: Change TaskToUnvoice decorator. Print task-stored costs.

--- app/models/concerns/task_costable.rb
+++ app/models/concerns/task_costable.rb
@@ -4,52 +4,45 @@ module TaskCostable
 
 
   def executor_rate_value
-    if stored_costs?
+    if task_level.hourly?
       stored_executor_rate_value
     else
-      if task_level.hourly?
-        task_level.executor_rate_value
-      end
+      task_level.executor_rate_value
     end
   end
 
   def client_rate_value
-    if stored_costs?
+    if task_level.hourly?
       stored_client_rate_value
     else
-      if task_level.hourly?
-        task_level.client_rate_value
-      end
+      task_level.client_rate_value
     end
   end
 
   def client_cost
-    if stored_costs?
+    if task_level.hourly?
       stored_client_cost
     else
-      if task_level.hourly?
-        task_level.client_rate_value * planned_time
-      end
+      task_level.client_rate_value * planned_time
     end
   end
 
   [:team_lead, :account_manager].each do |role|
     define_method "#{role}_rate_value" do
-      if stored_costs?
+      if task_level.hourly?
         self.send("stored_#{role}_rate_value")
       else
-        if task_level.hourly?
-          task_level.send("#{role}_rate_value")
-        end
+        task_level.send("#{role}_rate_value")
       end
     end
 
     define_method "#{role}_cost" do
-      rate_value = self.send("#{role}_rate_value")
-      if rate_value.present?
-        rate_value * planned_time
+      stored_value = self.send("stored_#{role}_cost")
+      if stored_value.present?
+        stored_value 
       else
-        nil
+        task_level.send("#{role}_rate_value")
+        rate_value * planned_time
       end
     end
   end
